3206c46aaf7b79375d7494d294e4d20634aa9f66
Igor Shyian <yourbestigor@gmail.com>
2021-07-29 16:22:39 +0300

    Renaming of login by facebook and google buttons

--- ftauth/src/main/java/com/fruktorum/ftauth/customUI/webView/FTLoginFacebookButton.kt
+++ ftauth/src/main/java/com/fruktorum/ftauth/customUI/webView/FTLoginFacebookButton.kt
@@ -5,14 +5,14 @@ import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatButton
 import com.fruktorum.ftauth.FTAuth
 
-class FTLoginByGoogleButton(
+class FTLoginFacebookButton(
     context: Context,
     attrs: AttributeSet?
 ) : AppCompatButton(context, attrs) {
 
     init {
         setOnClickListener {
-            FTAuth.getInstance().loginByGoogle(it.context)
+            FTAuth.getInstance().loginByFacebook(it.context)
         }
     }
 }
--- ftauth/src/main/java/com/fruktorum/ftauth/customUI/webView/FTLoginGoogleButton.kt
+++ ftauth/src/main/java/com/fruktorum/ftauth/customUI/webView/FTLoginGoogleButton.kt
@@ -5,14 +5,14 @@ import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatButton
 import com.fruktorum.ftauth.FTAuth
 
-class FTLoginByFacebookButton(
+class FTLoginGoogleButton(
     context: Context,
     attrs: AttributeSet?
 ) : AppCompatButton(context, attrs) {
 
     init {
         setOnClickListener {
-            FTAuth.getInstance().loginByFacebook(it.context)
+            FTAuth.getInstance().loginByGoogle(it.context)
         }
     }
 }
