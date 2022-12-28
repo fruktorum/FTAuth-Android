3d4ad4cc4a1911ab3b9c17db3bda6a120e3e1f5b
Andrey Yurin <a.yurin@agimagroup.ru>
2022-12-29 04:49:14 +0600

    #104-change formate of birhdate, add birthdate to payload

--- ftauth/src/main/java/com/fruktorum/ftauth/customUI/registration/FTRegistrationBirthdateField.kt
+++ ftauth/src/main/java/com/fruktorum/ftauth/customUI/registration/FTRegistrationBirthdateField.kt
@@ -14,6 +14,7 @@ import com.airbnb.paris.extensions.style
 import com.fruktorum.ftauth.FTAuth
 import com.fruktorum.ftauth.R
 import com.fruktorum.ftauth.customUI.common.FTAuthUI
+import com.fruktorum.ftauth.util.extensions.FORMAT_YYYY_MM_DD_T_HH_MM_SS
 import com.fruktorum.ftauth.util.extensions.setInputError
 import com.fruktorum.ftauth.util.extensions.setInputSuccess
 import java.text.SimpleDateFormat
@@ -121,8 +122,7 @@ class FTRegistrationBirthdateField @JvmOverloads constructor(
     }
 
     private fun updateLabel() {
-        val myFormat = "MM/dd/yy"
-        val dateFormat = SimpleDateFormat(myFormat, Locale.US)
+        val dateFormat = SimpleDateFormat(FORMAT_YYYY_MM_DD_T_HH_MM_SS, Locale.US)
         inputField.setText(dateFormat.format(calendar.time))
     }
 
---
+++ ftauth/src/main/java/com/fruktorum/ftauth/util/extensions/Date.kt
@@ -0,0 +1,6 @@
+package com.fruktorum.ftauth.util.extensions
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+val FORMAT_YYYY_MM_DD_T_HH_MM_SS = "yyyy-MM-dd'T'HH:mm:ssZZZ"
